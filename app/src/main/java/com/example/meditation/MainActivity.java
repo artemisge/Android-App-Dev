1b0e963605ea521fd519ac56b3d71442c437d21d
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-05-05 18:53:28 +0300

    testagain

--- app/src/main/java/com/example/meditation/MainActivity.java
+++ app/src/main/java/com/example/meditation/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // test 2
+    // test 22352
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
