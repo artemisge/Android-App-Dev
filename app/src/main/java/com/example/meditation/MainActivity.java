57234d94dffdf60a1f4878219849549c74182ccb
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-05-06 13:23:37 +0300

    activities added

--- app/src/main/java/com/example/meditation/MainActivity.java
+++ app/src/main/java/com/example/meditation/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.meditation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     // test 22352
@@ -11,4 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case  R.id.Calendar:
+                Intent intent = new Intent(MainActivity.this,Calendar.class;
+                startActivity(intent);
+                return true;
+            case R.id.Profile:
+                return true;
+            case R.id.Home:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
