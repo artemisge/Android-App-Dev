0bc1d718ad5d2f080f0565577be50cf0f86582dc
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-05-12 20:39:45 +0300

    set name works

--- app/src/main/java/com/example/meditation/Profile.java
+++ app/src/main/java/com/example/meditation/Profile.java
@@ -7,12 +7,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -23,8 +26,8 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        EditText et = findViewById(R.id.your_name);
-        et.setText("yoo");//MainActivity.dbHelper.getName());
+        TextView tv = findViewById(R.id.your_name);
+        tv.setText("Name");//MainActivity.dbHelper.getName());
         //MainActivity.dbHelper.changeName(et.getText().toString());
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.menu);
@@ -56,12 +59,17 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // guess what... EDIT NAME
+//                TextView tv = findViewById(R.id.your_name);
+//                tv.setVisibility(View.INVISIBLE);
+
                 EditText et = findViewById(R.id.your_name);
+                et.setFocusableInTouchMode(true);
+                et.setFocusable(true);
                 et.setEnabled(true);
+                et.setCursorVisible(true);
                 edit_name.setVisibility(View.GONE);
                 ImageButton edit_name_done = findViewById(R.id.edit_name_done);
                 edit_name_done.setVisibility(View.VISIBLE);
-                et.setCursorVisible(true);
             }
         });
 
@@ -71,9 +79,17 @@ public class Profile extends AppCompatActivity {
             public void onClick(View v) {
                 // guess what... EDIT NAME
                 EditText et = findViewById(R.id.your_name);
-                // update database
-                MainActivity.dbHelper.setName(et.getText().toString());
+                String new_name = et.getText().toString();
                 et.setEnabled(false);
+//                TextView tv = findViewById(R.id.your_name);
+//                tv.setVisibility(View.VISIBLE);
+//                et.setVisibility(View.GONE);
+//                tv.setText(new_name);
+                et.setFocusableInTouchMode(false);
+                et.setFocusable(false);
+                et.setCursorVisible(false);
+
+                //MainActivity.dbHelper.setName(et.getText().toString());
 
                 edit_name_done.setVisibility(View.GONE);
                 ImageButton edit_name = findViewById(R.id.edit_name);
@@ -81,24 +97,18 @@ public class Profile extends AppCompatActivity {
                 et.setCursorVisible(false);
             }
         });
-/*
+
         TextView set_reminder = findViewById(R.id.set_reminder);
         set_reminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //String string = MainActivity.dbHelper.getName();
                 Toast.makeText (Profile.this, "test: ", Toast.LENGTH_SHORT).show();
-                List<String> list = MainActivity.dbHelper.fetchData2();
-                Toast.makeText (Profile.this, list.toString(), Toast.LENGTH_LONG).show();
+                //List<String> list = MainActivity.dbHelper.fetchData2();
+                //Toast.makeText (Profile.this, list.toString(), Toast.LENGTH_LONG).show();
             }
-        });*/
+        });
 
-    }
-    // clickable Text View. Can't be assigned an OnClickListener :(
-    public void reminder_function(View v) {
-        Toast.makeText (Profile.this, "test: ", Toast.LENGTH_SHORT).show();
-        //List<String> list = MainActivity.dbHelper.fetchData2();
-        //Toast.makeText (Profile.this, list.toString(), Toast.LENGTH_LONG).show();
     }
 }
 
@@ -116,3 +126,17 @@ public class Profile extends AppCompatActivity {
 
             }
         });*/
+//
+//<TextView
+//    android:id="@+id/your_name"
+//    android:layout_width="wrap_content"
+//    android:layout_height="wrap_content"
+//    android:layout_gravity="center"
+//    android:layout_marginTop="56dp"
+//    android:fontFamily="cursive"
+//    android:text="Name"
+//    android:textColor="#757575"
+//    android:textSize="35sp"
+//    app:layout_constraintEnd_toEndOf="parent"
+//    app:layout_constraintStart_toStartOf="parent"
+//    app:layout_constraintTop_toBottomOf="@+id/imageButton" />
