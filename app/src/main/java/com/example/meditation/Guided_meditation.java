9c88c5c7f18df83816710c868461acfa0f48bb16
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-05-13 19:56:42 +0300

    chrysa merge done i hope

--- app/src/main/java/com/example/meditation/Guided_meditation.java
+++ app/src/main/java/com/example/meditation/Guided_meditation.java
@@ -1,16 +1,30 @@
 package com.example.meditation;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
 public class Guided_meditation extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList arrayList;
+    ArrayAdapter myAdapter;
+    MediaPlayer myMediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +38,20 @@ public class Guided_meditation extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.Calendar:
                         Intent intent1 = new Intent(Guided_meditation.this, Calendar.class);
+                        if(myMediaPlayer!=null)
+                            myMediaPlayer.release();
                         startActivity(intent1);
                         break;
                     case R.id.Profile:
                         Intent intent2 = new Intent(Guided_meditation.this, Profile.class);
+                        if(myMediaPlayer!=null)
+                            myMediaPlayer.release();
                         startActivity(intent2);
                         break;
                     case R.id.Home:
                         Intent intent3 = new Intent(Guided_meditation.this, MainActivity.class);
+                        if(myMediaPlayer!=null)
+                            myMediaPlayer.release();
                         startActivity(intent3);
                         break;
                 }
@@ -40,5 +60,27 @@ public class Guided_meditation extends AppCompatActivity {
             }
         });
 
+        listView = findViewById(R.id.listViewMed);
+        arrayList = new ArrayList<String>();
+        Field[] fields = R.raw.class.getFields();
+        for(int i=0; i<fields.length; i++){
+            arrayList.add(fields[i].getName());
+        }
+
+        myAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(myMediaPlayer!=null)
+                    myMediaPlayer.release();
+
+                int resId=getResources().getIdentifier((String) arrayList.get(position),"raw",getPackageName());
+                myMediaPlayer=MediaPlayer.create(Guided_meditation.this,resId);
+                myMediaPlayer.start();
+
+            }
+        });
     }
 }
